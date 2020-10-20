a96cfecce0fcdedf716ec4d9847d6fa7141065eb
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-20 12:33:00 +0100

    Button

---
+++ ultra-s/src/Components/pages/Button.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Button.css';
+
+const STYLES = ['btn-primary', 'btn--outline']
+
+function Button() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Button
