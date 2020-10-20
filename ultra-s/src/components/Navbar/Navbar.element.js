74c621e8dc46f6fdf236cbdcb76a296ae007504f
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-20 03:15:37 +0100

    nav

--- ultra-s/src/components/Navbar/Navbar.element.js
+++ ultra-s/src/components/Navbar/Navbar.element.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 export const Nav = styled.nav`
+
 background: #101522;
 height: 80px;
 display: flex;
@@ -9,5 +10,13 @@ align-items: center;
 font-size: 1.2rem;
 position: sticky;
 z-index: 999;
+`
 
+export const NavbarContainer = styled(Container)`
+
+display: flex;
+justify-content: space-between;
+height: 80px;
+
+$(Container)
 `
