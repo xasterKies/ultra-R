3ff6193cf635ab679fffaa00beb661b4fcff630e
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-20 17:38:40 +0100

    nav

--- ultra-s/src/Components/pages/Navbar.js
+++ ultra-s/src/Components/pages/Navbar.js
@@ -33,7 +33,7 @@ function Navbar() {
             <IconContext.Provider value={{ color: '#fff'}}>
            <div className="navbar">
                <div className="navbar-container container">
-                   <Link to='/' className="navbar-logo">
+                   <Link to='/' className="navbar-logo" onClick={closeMobileMenu}>
                        <MdFingerprint className='navbar-icon'/>
                        ULTRA
                    </Link>
