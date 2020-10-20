6d19be0c6d6019a7e4808a866632ab6bcde6a8d9
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-20 03:20:37 +0100

    global

---
+++ ultra-s/src/globalStyles.js
@@ -0,0 +1,10 @@
+import styled, {createGlobalStyle} from 'styled-component'
+
+const GlobalStyle = createGlobalStyle`
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Source Sans Pro', sans-serif;
+}
+`
