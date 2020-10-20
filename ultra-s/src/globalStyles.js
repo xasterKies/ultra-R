ca4240bb89906786f2cc9914c6cc5ab13d82167b
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-20 03:28:01 +0100

    global

--- ultra-s/src/globalStyles.js
+++ ultra-s/src/globalStyles.js
@@ -14,12 +14,12 @@ z-index: 1;
 width: 100%;
 max-width: 130px;
 margin-right: auto;
-margin-left: auto;
+padding-right: auto;
 padding-left: 50px;
 
 @media screen and (max-width: 991px) {
     padding-right: 30px;
-    padding
+    padding-left: 30px;
 }
 `
 
