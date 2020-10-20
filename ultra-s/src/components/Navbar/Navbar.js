63d8684803a7d1af242fd761b6491ac48f98dad3
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-20 03:12:30 +0100

    nav

--- ultra-s/src/components/Navbar/Navbar.js
+++ ultra-s/src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Nav } from './Navbar.element'
+import { Nav, NavbarContainer } from './Navbar.element'
 
 
 
@@ -7,10 +7,12 @@ const Navbar = () => {
     return (
         <>
             <Nav>
-                
-            <Nav/>
+                <NavbarContainer>
+
+                </NavbarContainer>
+            </Nav>
         </>
-    )
-}
+    );
+};
 
 export default Navbar
