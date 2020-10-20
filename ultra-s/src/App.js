06d9ad33455b1186da90fdb48605398703333af9
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-20 12:00:19 +0100

    app

--- ultra-s/src/App.js
+++ ultra-s/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navbar from './Components/pages/Navbar';
 
 
 
 function App() {
   return (
    <div className="App">
+     <Router>
+        <Navbar/>
+     </Router>
      
    </div>
   )
