62d698aac19415ff0b1a6c374386a202be4ed925
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-20 13:13:37 +0100

    btn

--- ultra-s/src/Components/pages/Navbar.js
+++ ultra-s/src/Components/pages/Navbar.js
@@ -2,11 +2,20 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { MdFingerprint } from 'react-icons/md'
 import { FaBars, FaTimes } from 'react-icons/fa'
+import { Button } from './Button'
 
 function Navbar() {
     const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true)
 
     const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false)
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        } else
+    }
 
     return (
         <>
