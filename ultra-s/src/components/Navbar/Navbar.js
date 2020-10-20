231223ac1f3e1ead72ef7f4100c22b087760caf1
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-20 03:02:54 +0100

    nav

--- ultra-s/src/components/Navbar/Navbar.js
+++ ultra-s/src/components/Navbar/Navbar.js
@@ -1,10 +1,23 @@
 import React from 'react'
 
+const Nav = styled.nav`
+background: #101522;
+height: 80px;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 1.2rem;
+position: sticky;
+z-index: 999;
+`
+
 const Navbar = () => {
     return (
-        <div>
+        <>
+        <nav>
             
-        </div>
+        </nav>
+        </>
     )
 }
 
