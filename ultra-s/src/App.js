eb3d58741cce1b5d2247738ac28994d2ea3cf634
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-20 03:54:00 +0100

    app

--- ultra-s/src/App.js
+++ ultra-s/src/App.js
@@ -5,9 +5,10 @@ import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar/>
-    </div>
+      
+    </Router>
   );
 }
 
