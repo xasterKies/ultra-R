33f7f101fc8a833dd442b62e9b1304277a14ba82
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-20 11:55:52 +0100

    nav

--- ultra-s/src/Components/pages/Navbar.js
+++ ultra-s/src/Components/pages/Navbar.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { MdFingerprint } 'react-icons/md '
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 function Navbar() {
     return (
@@ -6,6 +9,7 @@ function Navbar() {
            <div className="navbar">
                <div className="navbar-container container">
                    <Link to='/' className="navbar-logo">
+                       
                        ULTRA
                    </Link>
                    <div className="menu-icon">
