8113a2ecabbb7b2878e67043a38aef4e754bf36a
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-20 03:04:32 +0100

    styled

---
+++ ultra-s/src/components/Navbar/Navbar.element.js
@@ -0,0 +1,11 @@
+export const Nav = styled.nav`
+background: #101522;
+height: 80px;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 1.2rem;
+position: sticky;
+z-index: 999;
+
+`
--- ultra-s/src/components/Navbar/Navbar.js
+++ ultra-s/src/components/Navbar/Navbar.js
@@ -1,22 +1,13 @@
 import React from 'react'
 
-const Nav = styled.nav`
-background: #101522;
-height: 80px;
-display: flex;
-justify-content: center;
-align-items: center;
-font-size: 1.2rem;
-position: sticky;
-z-index: 999;
-`
+
 
 const Navbar = () => {
     return (
         <>
-        <nav>
-            
-        </nav>
+            <Nav>
+                
+            </Nav>
         </>
     )
 }
