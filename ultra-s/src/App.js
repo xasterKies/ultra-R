61731a2f43a3e7fbebc9d5e83bc6280c0604b75e
TiokengSamuel <samueledward40@gmail.com>
2021-04-03 23:21:18 +0100

    app.css

--- ultra-s/src/App.js
+++ ultra-s/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from './Components/pages/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.css'
+
 
 
 function App() {
