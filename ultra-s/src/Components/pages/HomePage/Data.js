78962f5466f173fb285c0ff8675d845d993724ae
TiokengSamuel <samueledward40@gmail.com>
2021-04-04 00:04:43 +0100

    homeObjOne data

---
+++ ultra-s/src/Components/pages/HomePage/Data.js
@@ -0,0 +1,12 @@
+export const homeObjOne = {
+    lightBg: false,
+    lightText: true,
+    lightTextDescs: true,
+    topLine: 'Exclusive Access',
+    headline: 'Unlimited Transaction with zero fees',
+    description: 'Get access to our exclusive diamond card that allows you to send unlimited transaction without getting charged any fees',
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    img: 'images/svg-1.svg',
+    alt: 'Credit Card'
+}
