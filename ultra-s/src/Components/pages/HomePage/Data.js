2fb7d56f3300803344e31394b434f688f254363f
TiokengSamuel <samueledward40@gmail.com>
2021-04-04 14:23:32 +0100

    reversing home object

--- ultra-s/src/Components/pages/HomePage/Data.js
+++ ultra-s/src/Components/pages/HomePage/Data.js
@@ -10,3 +10,42 @@ export const homeObjOne = {
     img: 'images/svg-1.svg',
     alt: 'Credit Card'
 }
+
+export const homeObjTwo = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: '100% SECURE',
+    headLine: 'Stay protected 24/7 anywhere anytime',
+    description: 'We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access',
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    img: 'images/svg-1.svg',
+    alt: 'Credit Card'
+}
+
+export const homeObjThree = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'EASY SETUP',
+    headLine: 'Super fast and simple onboarding process',
+    description: 'Get everything set up and ready in 10 minutes. All you need to do is add your information and you ready to go',
+    buttonLabel: 'Start Now',
+    imgStart: 'start',
+    img: 'images/svg-7.svg',
+    alt: 'Credit Card'
+}
+
+export const homeObjFour = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: '100% SECURE',
+    headLine: 'Stay protected 24/7 anywhere anytime',
+    description: 'We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access',
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    img: 'images/svg-1.svg',
+    alt: 'Credit Card'
+}
