2b51e65e0309b42f63b948c9ab4ecdcac248649f
TiokengSamuel <samueledward40@gmail.com>
2021-04-04 14:49:24 +0100

    four section

--- ultra-s/src/Components/pages/HomePage/Data.js
+++ ultra-s/src/Components/pages/HomePage/Data.js
@@ -18,9 +18,9 @@ export const homeObjTwo = {
     topLine: '100% SECURE',
     headLine: 'Stay protected 24/7 anywhere anytime',
     description: 'We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access',
-    buttonLabel: 'Get Started',
+    buttonLabel: 'Learn More',
     imgStart: '',
-    img: 'images/svg-1.svg',
+    img: 'images/svg-5.svg',
     alt: 'Credit Card'
 }
 
@@ -41,11 +41,11 @@ export const homeObjFour = {
     lightBg: false,
     lightText: true,
     lightTextDesc: true,
-    topLine: '100% SECURE',
-    headLine: 'Stay protected 24/7 anywhere anytime',
-    description: 'We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access',
-    buttonLabel: 'Get Started',
-    imgStart: '',
-    img: 'images/svg-1.svg',
+    topLine: 'DATA ANALYTICS',
+    headLine: 'Every transaction is stored on our secure cloud database',
+    description: 'Never ever have to worry again about saved reciepts. we store your data, so you can access it anytime.',
+    buttonLabel: 'Sign Up Now',
+    imgStart: 'start',
+    img: 'images/svg-6.svg',
     alt: 'Credit Card'
 }
