d15454ee0bf57a9fa973afa7442f04dcb1bab504
TiokengSamuel <samueledward40@gmail.com>
2021-04-05 18:41:19 +0100

    Pricing Section

--- ultra-s/src/Components/pages/Pricing.js
+++ ultra-s/src/Components/pages/Pricing.js
@@ -5,7 +5,7 @@ function Pricing() {
         <div>
             <div className="pricing__section">
                 <div className="pricing__wrapper">
-                    .pri
+                    .
                 </div>
             </div>
         </div>
