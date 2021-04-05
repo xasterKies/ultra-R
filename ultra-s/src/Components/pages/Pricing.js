4b88281355784929f7558326a96fec82a8bec1a7
TiokengSamuel <samueledward40@gmail.com>
2021-04-05 19:28:55 +0100

    color button to blue

--- ultra-s/src/Components/pages/Pricing.js
+++ ultra-s/src/Components/pages/Pricing.js
@@ -46,7 +46,7 @@ function Pricing() {
                                 <li>3.5% cash back</li>
                                 <li>$100,000 Limit</li>
                             </ul>
-                            <Button buttonSize='btn--wide' buttonColor='primary'>
+                            <Button buttonSize='btn--wide' buttonColor='blue'>
                                 Choose Plan
                             </Button>
                         </Link>
