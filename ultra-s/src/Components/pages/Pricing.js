ceed69261c2591c431c3c9723b3a254ed32387b7
TiokengSamuel <samueledward40@gmail.com>
2021-04-05 06:33:59 +0100

    starting pricing

---
+++ ultra-s/src/Components/pages/Pricing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Pricing() {
+    return (
+        <div>
+            <div className="pricing__section">
+                <div className="pricing__wrapper">
+                    .pri
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Pricing
