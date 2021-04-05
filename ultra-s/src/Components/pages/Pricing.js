a54b456d18c246450a08c32148b78c700690c149
TiokengSamuel <samueledward40@gmail.com>
2021-04-05 18:49:16 +0100

    one size card

--- ultra-s/src/Components/pages/Pricing.js
+++ ultra-s/src/Components/pages/Pricing.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import { Button } from './Button'
 
 function Pricing() {
     return (
         <div>
             <div className="pricing__section">
                 <div className="pricing__wrapper">
-                    .
+                    <h1 className="pricing__heading">Pricing</h1>
+                    <div className="pricing__container">
+                        <Link to="/sign-up"
+                         className='pricing__container-cardInfo'>
+                            <div className="icon">
+                                <FaFire/>
+                            </div>
+                            <h3>Starter</h3>
+                            <h4>$8.99</h4>
+                            <p>per month</p>
+                            <ul className="pricing__container-feature">
+                                <li>100 Transactions</li>
+                                <li>2% cash back</li>
+                                <li>$10,000 Limit</li>
+                            </ul>
+                            <Button buttonSize='btn--wide' buttonColor='primary'>
+                                Choose Plan
+                            </Button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
