f05d992c0455015eb7208b0c311edf36e61b429e
TiokengSamuel <samueledward40@gmail.com>
2021-04-05 18:58:39 +0100

    morkuping 3rd card

--- ultra-s/src/Components/pages/Pricing.js
+++ ultra-s/src/Components/pages/Pricing.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaFire } from 'react-icons/fa'
+import { BsXDiamondFill } from 'react-icons/bs'
 import { Button } from './Button'
 
 function Pricing() {
@@ -19,6 +22,40 @@ function Pricing() {
                             <ul className="pricing__container-feature">
                                 <li>100 Transactions</li>
                                 <li>2% cash back</li>
+                                <li>$10,000 Limit</li>
+                            </ul>
+                            <Button buttonSize='btn--wide' buttonColor='primary'>
+                                Choose Plan
+                            </Button>
+                        </Link>
+                        <Link to="/sign-up"
+                         className='pricing__container-cardInfo'>
+                            <div className="icon">
+                                <BsXDiamondFill/>
+                            </div>
+                            <h3>Gold</h3>
+                            <h4>$29.99</h4>
+                            <p>per month</p>
+                            <ul className="pricing__container-feature">
+                                <li>1000 Transactions</li>
+                                <li>3.5% cash back</li>
+                                <li>$100,000 Limit</li>
+                            </ul>
+                            <Button buttonSize='btn--wide' buttonColor='primary'>
+                                Choose Plan
+                            </Button>
+                        </Link>
+                        <Link to="/sign-up"
+                         className='pricing__container-cardInfo'>
+                            <div className="icon">
+                                <GiCrystalize/>
+                            </div>
+                            <h3>Diamond</h3>
+                            <h4>$99.99</h4>
+                            <p>per month</p>
+                            <ul className="pricing__container-feature">
+                                <li> Unlimited Transactions</li>
+                                <li>5% cash back</li>
                                 <li>$10,000 Limit</li>
                             </ul>
                             <Button buttonSize='btn--wide' buttonColor='primary'>
